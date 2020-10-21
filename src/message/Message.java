d7ebd936c64a2b702f5a7ae16f955ec9dec17ef0
2dam <2dam@HZ359575.TartangaLH.eus>
2020-10-21 10:04:17 +0200

    Message implemented

---
+++ src/message/Message.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package message;
+
+/**
+ *
+ * @author Lorena Fdez Iñigo
+ */
+public class Message {
+    enum Messages{
+        SIGN_IN,
+        SIGN_UP,
+        SIGN_OUT,
+        USER_NOT_FOUND,
+        PASSWORD_MATCH_ERROR
+    }
+    
+}
