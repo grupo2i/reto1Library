58ec0ca4dd093a27ce1ca86ec22ad5f748b9fa40
2dam <2dam@HZ359590.TartangaLH.eus>
2020-11-13 08:18:23 +0100

    No response from server window showing implemented.

--- src/exceptions/UnexpectedErrorException.java
+++ src/exceptions/UnexpectedErrorException.java
@@ -9,6 +9,7 @@ import java.util.logging.Logger;
  */
 public class UnexpectedErrorException extends Exception{
     public UnexpectedErrorException(String exceptionMessage){
+        super("No response from server.");
         Logger.getLogger(UnexpectedErrorException.class.getName()).log(Level.SEVERE, exceptionMessage);
     }
 }
